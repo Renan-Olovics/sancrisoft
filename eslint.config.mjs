5a1870d5c83da4a54802a075ff9cc3a2c6bc9683
renan olovics <renanolovics@gmail.com>
2025-07-18 10:24:08 -0300

    chore: initial config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,98 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
+import importPlugin from 'eslint-plugin-import'
+import preferArrowFunctions from 'eslint-plugin-prefer-arrow-functions'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'prefer-arrow-functions': preferArrowFunctions,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'prefer-arrow-functions/prefer-arrow-functions': [
+        'error',
+        {
+          allowedNames: [],
+          allowNamedFunctions: false,
+          allowObjectProperties: false,
+          classPropertiesAllowed: false,
+          disallowPrototype: false,
+          returnStyle: 'unchanged',
+          singleReturnOnly: false,
+        },
+      ],
+      'no-dupe-keys': 'error',
+      'max-len': [
+        'error',
+        {
+          code: 100,
+          tabWidth: 2,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+      'no-unneeded-ternary': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        {
+          prefer: 'type-imports',
+          disallowTypeAnnotations: false,
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin', // next, react, node
+            'external', // libs
+            'internal', // @/
+            ['parent', 'sibling', 'index'], // ../../, ./
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: 'next',
+              group: 'builtin',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  ...compat.extends('plugin:prettier/recommended'),
+]
 
-export default eslintConfig;
+export default eslintConfig

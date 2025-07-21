32a34e1ab4d0fe5244ce5663c3b793e24dd7b889
Renan Olovics <renanolovics@gmail.com>
2025-07-21 12:57:32 -0300

    chore: removed props

--- src/components/atoms/Icon/component.tsx
+++ src/components/atoms/Icon/component.tsx
@@ -4,11 +4,11 @@ import * as Icons from '@/assets'
 
 import type { IconProps } from './types'
 
-export const Icon = ({ name, size = 50, width, height, className, ...props }: IconProps) => {
+export const Icon = ({ name, size = 50, width, height, ...props }: IconProps) => {
   const IconComponent = Icons[name]
   if (!IconComponent) return null
 
   return (
-    <IconComponent width={width ?? size} height={height ?? size} className={className} {...props} />
+    <IconComponent width={width ?? size} height={height ?? size} {...props} />
   )
 }
